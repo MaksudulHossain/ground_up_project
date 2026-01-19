ec3ee2615c95d00065dbbee1e020c20702ee1556
MaksudulHossain <jewelmaksud@gmail.com>
2026-01-19 12:01:37 -0800

    New feature added: writing to a file

--- ground_up.py
+++ ground_up.py
@@ -1,9 +1,17 @@
 def calculate_force(m,a):
     return m*a 
+
 try:
     m = float(input("mass= "))
     a = float(input("acc= "))
     F = calculate_force(m,a)
-    print(F)
+    # print(F)
+
+    with open("results.txt", "a") as f:
+        f.write(f"m={m}, a={a}, F={F}\n")
+
+    print(f"Force: {F}")
+    print("Saved to resuls.txt")
+    
 except ValueError:
     print("Invalidd input. Please enter numeric val.")
